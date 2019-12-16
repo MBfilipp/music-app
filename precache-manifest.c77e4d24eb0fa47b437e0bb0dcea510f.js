721c57c1e9e308b503a1d9ce1fb295ce7fc5698a
Filipp <52923549+MBfilipp@users.noreply.github.com>
2019-12-16 21:18:37 +0300

    Updates

---
+++ precache-manifest.c77e4d24eb0fa47b437e0bb0dcea510f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a9352d2083904a40afcaf144a12934",
+    "url": "/music-app/index.html"
+  },
+  {
+    "revision": "9d502bff4cf500c4a3dc",
+    "url": "/music-app/static/css/main.793c5b59.chunk.css"
+  },
+  {
+    "revision": "d98cf9754b25a5f8e6b6",
+    "url": "/music-app/static/js/2.f2dd9402.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/music-app/static/js/2.f2dd9402.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d502bff4cf500c4a3dc",
+    "url": "/music-app/static/js/main.e4cd94f2.chunk.js"
+  },
+  {
+    "revision": "999cd381c4e50711dabc",
+    "url": "/music-app/static/js/runtime-main.8b7f9585.js"
+  }
+]);
